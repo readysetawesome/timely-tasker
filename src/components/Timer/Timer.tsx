24a074a823bcfd7bfb56081be707b82660428b8f
Christopher Becker <cbecker333@gmail.com>
2023-03-13 08:24:57 -0700

    add access plugin and function stubs

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,12 +1,49 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styles from './Timer.module.scss';
 
 interface TimerProps {}
 
-const Timer: FC<TimerProps> = () => (
-  <div className={styles.Timer}>
-    Timer Component
-  </div>
-);
+const Timer: FC<TimerProps> = () => {
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    
+    fetch('/greet', {mode:'cors'})
+        .then(async  response => {
+          const foo = await response.text();
+          setGreeting(foo);
+        });
+  });
+
+  return (
+    <div className={styles.Timer}>
+      Timer Component
+      <p>{greeting || 'loading...'}</p>
+      <table>
+        <thead>
+          <tr>
+            <th className="left">TASKS</th>
+            <th className="right">START TIME</th>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            <th>Column 3</th>
+            <th>Column 4</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td colSpan={2}>_________________</td>
+            <td>Content 1</td>
+            <td>Content 2</td>
+            <td>Content 3</td>
+            <td>Content 4</td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+  )
+};
 
 export default Timer;
