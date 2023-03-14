271cf2cdec595609e3eebe07c864b7e3728eba73
Christopher Becker <cbecker333@gmail.com>
2023-03-13 17:42:49 -0700

    set audience from env context

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,8 +1,20 @@
 import cloudflareAccessPlugin from "@cloudflare/pages-plugin-cloudflare-access";
 
-export const onRequest = ({ env }) => {
-  return cloudflareAccessPlugin({
-    domain: "https://timely-tasker.cloudflareaccess.com",
-    aud: env.AUDIENCE,
-  });
+interface Env {
+  AUDIENCE: string;
 }
+
+export const onRequest: PagesFunction<Env> = (context) => {
+  const aud = context.env.AUDIENCE;
+  if (typeof aud !== 'string') {
+    // must be local dev mode, just pass through
+    // TODO: Stub local dev user auth in the middeware/functions
+    return context.next();
+  } else {
+    return cloudflareAccessPlugin({
+      domain: "https://timely-tasker.cloudflareaccess.com",
+      aud: aud
+    })(context);
+  }
+}
+
--- functions/greet.ts
+++ functions/greet.ts
@@ -3,7 +3,9 @@ import type { PluginData } from "@cloudflare/pages-plugin-cloudflare-access";
 export const onRequest: PagesFunction<unknown, any, PluginData> = async ({
   data,
 }) => {
-  const identity = await data.cloudflareAccess.JWT.getIdentity();
+  const identity = typeof data.cloudflareAccess !== "undefined" ?
+    await data.cloudflareAccess.JWT.getIdentity() :
+    {name: "dev user"}
 
   return new Response(`Hello, ${identity.name || "service user"}!`);
 };
