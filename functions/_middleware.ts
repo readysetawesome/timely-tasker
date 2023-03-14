e016041a1d5b83693282a43eb9436557bbbe0c18
Christopher Becker <cbecker333@gmail.com>
2023-03-14 08:16:04 -0700

    fix react-scripts version. cleanup component syntax, no need for FC typings.

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,6 +1,45 @@
 import cloudflareAccessPlugin from "@cloudflare/pages-plugin-cloudflare-access";
 
-export const onRequest: PagesFunction = cloudflareAccessPlugin({
-  domain: "https://timely-tasker.cloudflareaccess.com",
-  aud: "d2ed32aa3e70054195dc74af294e9ea7b8d76f36a6041bdb6ef3ca7d8c39841b",
-});
+interface Env {
+  AUDIENCE: string;
+}
+
+// Respond to OPTIONS method
+// TODO only enable this for dev until needed otherwise
+export const onRequestOptions: PagesFunction = async () => {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Max-Age': '86400',
+    },
+  });
+};
+
+// Set CORS to all /api responses
+// TODO only enable this for dev until needed otherwise
+const onRequestCors: PagesFunction = async ({ next }) => {
+  const response = await next();
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Max-Age', '86400');
+  return response;
+};
+
+
+const onRequestDoPlugin: PagesFunction<Env> = (context) => {
+  const aud = context.env.AUDIENCE;
+  if (typeof aud !== 'string') {
+    // must be local dev mode, just pass through
+    // TODO: Stub local dev user auth in the middeware/functions
+    return context.next();
+  } else {
+    return cloudflareAccessPlugin({
+      domain: "https://timely-tasker.cloudflareaccess.com",
+      aud: aud
+    })(context);
+  }
+}
+
+export const onRequest = [onRequestCors, onRequestDoPlugin]
--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,49 +1,37 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Timer.module.scss';
 
 interface TimerProps {}
 
-const Timer: FC<TimerProps> = () => {
+const Timer = ({ TimerProps }) => {
   const [greeting, setGreeting] = useState("");
 
   useEffect(() => {
-    
-    fetch('/greet', {mode:'cors'})
+    // TODO for local dev mode prefix with http://127.0.0.1:8788
+    fetch('http://127.0.0.1:8788/greet', {mode:'cors'})
         .then(async  response => {
           const foo = await response.text();
           setGreeting(foo);
         });
   });
 
-  return (
+  return <>
+    <div>{greeting || 'loading...'}</div>
     <div className={styles.Timer}>
       Timer Component
-      <p>{greeting || 'loading...'}</p>
-      <table>
-        <thead>
-          <tr>
-            <th className="left">TASKS</th>
-            <th className="right">START TIME</th>
-            <th>Column 1</th>
-            <th>Column 2</th>
-            <th>Column 3</th>
-            <th>Column 4</th>
 
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td colSpan={2}>_________________</td>
-            <td>Content 1</td>
-            <td>Content 2</td>
-            <td>Content 3</td>
-            <td>Content 4</td>
-          </tr>
-        </tbody>
-      </table>
-
+      <div className={styles.grid}>
+        <div className={styles.tictac}></div>
+        <div className={styles.tictac}></div>
+        <div className={styles.tictac}></div>
+        <div className={styles.tictac}></div>
+        <div className={styles.tictac}></div>
+        <div className={styles.tictac}></div>
+        <div className={styles.tictac}></div>
+        <div className={styles.tictac}></div>
+      </div>
     </div>
-  )
+  </>
 };
 
 export default Timer;
