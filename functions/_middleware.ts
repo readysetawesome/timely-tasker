7992d6ae100cac41a3dc9823f92dc324ee90de4c
Christopher Becker <cbecker333@gmail.com>
2023-03-14 08:16:04 -0700

    fix react-scripts version. cleanup component syntax, no need for FC typings.

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -4,7 +4,30 @@ interface Env {
   AUDIENCE: string;
 }
 
-export const onRequest: PagesFunction<Env> = (context) => {
+// Respond to OPTIONS method
+// TODO only enable this for dev until needed otherwise
+export const onRequestOptions: PagesFunction = async () => {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Max-Age': '86400',
+    },
+  });
+};
+
+// Set CORS to all /api responses
+// TODO only enable this for dev until needed otherwise
+const onRequestCors: PagesFunction = async ({ next }) => {
+  const response = await next();
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Max-Age', '86400');
+  return response;
+};
+
+const onRequestDoPlugin: PagesFunction<Env> = (context) => {
   const aud = context.env.AUDIENCE;
   if (typeof aud !== 'string') {
     // must be local dev mode, just pass through
@@ -18,3 +41,4 @@ export const onRequest: PagesFunction<Env> = (context) => {
   }
 }
 
+export const onRequest = [onRequestCors, onRequestDoPlugin]
