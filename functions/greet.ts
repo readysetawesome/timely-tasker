11eb7a1639f6d8d1cc4bcc196b4e138cd5e6f89a
Christopher Becker <cbecker333@gmail.com>
2023-03-15 13:00:09 -0700

    fix initial greeting lazy ident init response, include full db identity

--- functions/greet.ts
+++ functions/greet.ts
@@ -14,7 +14,9 @@ const errorResponse = (error: string) => new Response(JSON.stringify({ error }),
 export const onRequest: PagesFunction<Env, any, PluginData> = async ({
   data, env
 }) => {
-  const { identity, jwtIdentity, provider, error } = await GetIdentity(data, env);
+  const result = await GetIdentity(data, env);
+  let { identity } = result;
+  const { jwtIdentity, provider, error } = result;
 
   if (error) return errorResponse(error);
 
@@ -23,26 +25,36 @@ export const onRequest: PagesFunction<Env, any, PluginData> = async ({
     // Why? Users will forget which provider they logged in with.
     // If their data "disappears" because they switch providers, they are confused.
 
-    const existingUser: any = await env.DB.prepare(`
+    const userQuery = env.DB.prepare(`
       SELECT * FROM  Identities, Providers, Users
       WHERE Email = ?
         AND Users.ID = UserID
         AND Providers.ID = ProviderID
-    `).bind(jwtIdentity.email).first();
+    `);
+
+    const existingUser: any = await userQuery.bind(jwtIdentity.email).first();
 
     if (existingUser) return errorResponse(`
       A user already signed up with this email address using ${existingUser.ProviderName}.
       Was it you? If it was you, try logging in with ${existingUser.ProviderName} instead.
     `);
 
-    const results = await env.DB.batch([
+    const batch = await env.DB.batch([
       env.DB.prepare(`INSERT INTO Users (DisplayName, Email) values (?, ?)`)
         .bind(jwtIdentity.name, jwtIdentity.email),
       env.DB.prepare(`INSERT INTO Identities (ProviderID, UserID, ProviderIdentityID) values (?, last_insert_rowid(), ?)`)
-        .bind(provider.ID, jwtIdentity.user_uuid)
+        .bind(provider.ID, jwtIdentity.user_uuid),
+      env.DB.prepare(`
+        SELECT * FROM Identities, Providers, Users
+        WHERE Users.ID = UserID
+          AND Providers.ID = ProviderID
+          AND Identities.ID = last_insert_rowid()
+      `),
     ])
 
-    if (!results[1].success) return errorResponse("unable to insert new  User & Identity mapping");
+    if (!batch[1].success) return errorResponse("unable to insert new  User & Identity mapping");
+
+    identity = batch[2].results?.[0];
   }
 
   return new Response(JSON.stringify(identity, null, 2), JsonHeader);
